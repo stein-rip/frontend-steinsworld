ca34c48e493b96d1a191df85f97d3824b0c076a7
Stein_ <stein@stein.rip>
2024-01-21 20:26:07 -0500

    did i do this right this time

---
+++ src/App.js
@@ -0,0 +1,43 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+<html lang="en">
+	<head>
+		<meta charset="UTF-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<link rel="stylesheet" href="style.css" />
+		<title>stein.rip</title>
+	</head>
+	<body>
+		<h1>:</h1>
+
+		<div id="background-container">
+			<img id="bgImage1" src="assets/stein-suck.png" alt="glitch"/>
+			<img id="bgImage2" src="assets/stein-good.png" alt="solid" />
+		</div>
+		<main>
+			<div className="bread">
+				<a href="http://breadwinners-fork.web.app" alt="bread"></a>
+			</div>
+
+			<div className="mem">
+				<a href="http://mem-portal.surge.sh"></a>
+			</div>
+		</main>
+
+		<footer>
+			<img className="toilet" src="assets/toiletstein.png" />
+			<h6>STEIN 2023</h6>
+		</footer>
+		<script src="script.js"></script>
+	</body>
+</html>
+
+    </div>
+  );
+}
+
+export default App;
